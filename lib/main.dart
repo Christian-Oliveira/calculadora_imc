28760ddc22e6b900ce75a76f744187b1d4949e1f
Christian-Oliveira <christianoliveirati@gmail.com>
2019-06-26 16:53:12 -0300

    botao de calculo de imc ok

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,32 @@ class _HomeState extends State<Home> {
   String _textInfor = "Informe seus dados!";
 
   void _resetFields(){
-    weightController.text = "";
-    heightController.text = "";
-    _textInfor = "Informe seus dados!";
+    setState(() {
+       weightController.text = "";
+      heightController.text = "";
+      _textInfor = "Informe seus dados!";
+    });
   }
 
   void _calculate(){
-     double weight = double.parse(weightController.text);
-     double height = double.parse(heightController.text) / 100;
-     double imc = weight / (height * height);
-     print(imc);
+    double weight = double.parse(weightController.text);
+    double height = double.parse(heightController.text) / 100;
+    double imc = weight / (height * height);
+    setState(() {
+      if(imc < 18.6){
+       _textInfor = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+     } else if(imc >= 18.6 && imc < 24.9){
+       _textInfor = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+     } else if(imc >= 24.9 && imc < 29.9){
+       _textInfor = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+     } else if(imc >= 29.9 && imc < 34.9){
+       _textInfor = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+     } else if(imc >= 34.9 && imc < 39.9){
+       _textInfor = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+     } else if(imc >= 40){
+       _textInfor = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+     }   
+    });
   }
 
   @override
